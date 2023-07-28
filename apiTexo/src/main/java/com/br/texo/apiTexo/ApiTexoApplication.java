2b8246974fdac314db6980a6911ba4fa393c4239
ramosrenan <renan.barbosards@gmail.com>
2023-07-28 15:06:59 -0300

    Comitando criação da aplicação

---
+++ apiTexo/src/main/java/com/br/texo/apiTexo/ApiTexoApplication.java
@@ -0,0 +1,13 @@
+package com.br.texo.apiTexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTexoApplication.class, args);
+	}
+
+}
---
+++ apiTexo/src/test/java/com/br/texo/apiTexo/ApiTexoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.texo.apiTexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
