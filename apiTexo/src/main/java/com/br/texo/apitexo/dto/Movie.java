cc0460b43c0d4ac8ba338ce1a6b30da0e57057b5
ramosrenan <renan.barbosards@gmail.com>
2023-08-06 21:05:54 -0300

    Adicionado classe movie dto

---
+++ apiTexo/src/main/java/com/br/texo/apitexo/dto/Movie.java
@@ -0,0 +1,9 @@
+package com.br.texo.apitexo.dto;
+
+public class Movie
+{
+    public String   producer        = "";
+    public int      interval        = 0;
+    public int      previousWin     = 0;
+    public int      followingWin    = 0;
+}
