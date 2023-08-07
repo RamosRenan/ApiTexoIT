d512f653f67a6ca6ce399af773cc1b75f2df9f99
ramosrenan <renan.barbosards@gmail.com>
2023-08-06 21:13:53 -0300

    Adicionado movie repository para dml

--- apiTexo/src/main/java/com/br/texo/apitexo/repositories/MovieRepository.java
+++ apiTexo/src/main/java/com/br/texo/apitexo/repositories/MovieRepository.java
@@ -1,13 +1,15 @@
 package com.br.texo.apitexo.repositories;
 
 import com.br.texo.apitexo.entities.MovieEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
-public interface MovieRepository extends JpaRepository<MovieEntity, Integer>
+public interface MovieRepository extends Repository<MovieEntity, Integer>
 {
+    public Page<MovieEntity> findAll(Pageable pageable);
 
+    public List<MovieEntity> findByWinner(String isWinner);
 }
