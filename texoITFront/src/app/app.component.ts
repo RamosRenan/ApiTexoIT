c77dce1e729179b8c4910622fd94e9e6701fdd9d
ramos.renan <renan.barbosards@gmail.com>
2023-08-09 07:06:44 -0300

    Criando telas

--- texoITFront/src/app/app.component.ts
+++ texoITFront/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'texoITFrontEnd';
+export class AppComponent
+{
+  showFiller = false;
 }
